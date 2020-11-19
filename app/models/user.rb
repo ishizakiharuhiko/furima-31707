0a5e722f776994826781c4c697b14059117357c2
ishizakiharuhiko <73152253+ishizakiharuhiko@users.noreply.github.com>
2020-11-19 18:54:38 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true
+  validates :encrypted_password, presence: true, format: { with: /\A[a-z\d]{8,}/i\z/}
+  validates :nickname, presence: true
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :kana_first_name, presence: true, format: { with: /\A[ァ-ン]+\z/}
+  validates :kana_last_name, presence: true, format: { with: /\A[ァ-ン]+\z/}
+  validates :birthday, presence: true
 end

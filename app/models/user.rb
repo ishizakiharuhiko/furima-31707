416521dac3d675633c0c834539a05ce1b8f76ebc
ishizakiharuhiko <73152253+ishizakiharuhiko@users.noreply.github.com>
2020-11-20 17:20:47 +0900

    修正２

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}+\z/i }
-  validates :nickname, presence: true
-  validates :first_name, presence: true, format: { with: /\A[[ぁ-んァ-ン一-龥]]+\z/ }
-  validates :last_name, presence: true, format: { with: /\A[[ぁ-んァ-ン一-龥]]+\z/ }
-  validates :kana_first_name, presence: true, format: { with: /\A[ァ-ン]+\z/ }
-  validates :kana_last_name, presence: true, format: { with: /\A[ァ-ン]+\z/ }
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}+\z/i }
+    validates :nickname
+    validates :first_name, format: { with: /\A[[ぁ-んァ-ン一-龥]]+\z/ }
+    validates :last_name, format: { with: /\A[[ぁ-んァ-ン一-龥]]+\z/ }
+    validates :kana_first_name, format: { with: /\A[ァ-ン]+\z/ }
+    validates :kana_last_name, format: { with: /\A[ァ-ン]+\z/ }
+    validates :birthday
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,12 +9,6 @@ describe User do
       it 'emailとpassword、password_confirmationが存在すれば登録できる' do
         expect(@user).to be_valid
       end
-      it 'passwordが6文字以上であれば登録できる' do
-        @user.password = '111aaa'
-        @user.encrypted_password = '111aaa'
-        @user.password_confirmation = '111aaa'
-        expect(@user).to be_valid
-      end
     end
 
     context '新規登録がうまくいかないとき' do
@@ -46,9 +40,15 @@ describe User do
         @user.valid?
         expect(@user.errors.full_messages).to include("Password can't be blank")
       end
-      it 'passwordが半角英数混合でなければ登録できない' do
+      it 'passwordが半角数字のみでは登録できない' do
         @user.password = '111111'
         @user.password_confirmation = '111111'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end
+      it 'passwordが半角英語のみでは登録できない' do
+        @user.password = 'aaaaaa'
+        @user.password_confirmation = 'aaaaaa'
         @user.valid?
         expect(@user.errors.full_messages).to include("Password is invalid")
       end

a32577b0c02b5130c2e270767889c25dc0a4c15a
ishizakiharuhiko <73152253+ishizakiharuhiko@users.noreply.github.com>
2020-11-23 18:21:38 +0900

    ストロングパラメーター、アソシエーション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,12 @@
 class ItemsController < ApplicationController
   def index
   end
+
+
+  private
+
+  def items_params
+    params.require(:item).permit(:title, :text, :category_id, :status_id, :shipping_id, :prefecture_id, :day_id, :price,:image).merge(user_id: current_user.id)
+  end
+
 end

bfcf12fe4cc9d4d2ab323885112735f41068522d
ishizakiharuhiko <73152253+ishizakiharuhiko@users.noreply.github.com>
2020-11-30 20:52:07 +0900

    商品情報の受け渡し、リダイレクト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :set_item, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :contributor_confirmation, except:[:index, :show]
 
   def index
     @items = Item.order('created_at DESC')
@@ -19,6 +21,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def show
@@ -26,6 +29,12 @@ class ItemsController < ApplicationController
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(items_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -33,9 +42,17 @@ class ItemsController < ApplicationController
 
   private
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
   def items_params
     params.require(:item).permit(
       :title, :text, :category_id, :status_id, :shipping_id, :prefecture_id, :day_id, :price, :image
     ).merge(user_id: current_user.id)
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @item.user
   end
 end

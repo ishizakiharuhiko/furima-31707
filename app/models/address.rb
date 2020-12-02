144e6f6ef55c3b659bed668e07f9f0a319b78e16
ishizakiharuhiko <73152253+ishizakiharuhiko@users.noreply.github.com>
2020-12-03 05:22:06 +0900

    バリデーションの追記

--- app/models/address.rb
+++ app/models/address.rb
@@ -5,11 +5,11 @@ class Address < ApplicationRecord
   belongs_to :order
 
   with_options presence: true do
-    validates :postal_number
+    validates :postal_number, format: { with: /\A\d{3}[-]\d{4}\z/ }
     validates :prefecture_id
     validates :city
     validates :street
-    validates :telephone_number
+    validates :telephone_number, numericality: { only_integer: true }, length: { maximum: 11 }
   end
 
   validates :building_name

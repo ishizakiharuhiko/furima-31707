55600b415279ce6c5a705504cbae0eb5486c3e20
ishizakiharuhiko <73152253+ishizakiharuhiko@users.noreply.github.com>
2020-12-03 05:09:40 +0900

    アソシエーション

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,16 @@
 class Address < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+
+  belongs_to :order
+
+  with_options presence: true do
+    validates :postal_number
+    validates :prefecture_id
+    validates :city
+    validates :street
+    validates :telephone_number
+  end
+
+  validates :building_name
 end
